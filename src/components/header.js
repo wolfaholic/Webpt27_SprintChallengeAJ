dd947ea35a5edf7ac68c27af5e4b9ab819f0d1b2
Melissa <wolfaholic@users.noreply.github.com>
2021-03-21 18:08:11 -0700

    header complete

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,5 @@
 const Header = (title, date, temp) => {
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
+  
   const headDiv = document.createElement('div');
   const headDate = document.createElement('span');
   const headTitle = document.createElement('h1');
@@ -27,17 +16,35 @@ const Header = (title, date, temp) => {
   headDate.textContent = title;
   headTitle.textContent = date;
   headTemp.textContent = temp;
- 
+  
+  
+  // TASK 1
+  // ---------------------
+  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
+  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
+  //
+  //  <div class="header">
+  //    <span class="date">{ date }</span>
+  //    <h1>{ title }</h1>
+  //    <span class="temp">{ temp }</span>
+  //  </div>
+  //
 
+  return headDiv;
 }
 
 const headerAppender = (selector) => {
+let headContainer = document.querySelector(selector);
+  headContainer.append(Header('The Lambda Times', '1/23/2021', '26F'))
+}
+
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-}
+ 
 
 export { Header, headerAppender }
